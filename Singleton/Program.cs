d4d404e7f58728a29f1e71c582637844880327ee
Emmanuel Zelarayan <emmavz@live.com>
2023-03-21 20:46:07 -0300

    add singleton pattern

---
+++ Singleton/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var singleton = Singleton.Instance;
+            var log = Log.Instance;
+            log.Save("a");
+            log.Save("b");
+
+            var a = Singleton.Instance;
+            var b = Singleton.Instance;
+
+            Console.WriteLine(a == b);
+
+            Console.ReadKey();
+        }
+    }
+}
